10f6964b7bc8843518c21f6be8de2cc5a5bff5c9
mabafaba <m@martinbarner.de>
2025-09-17 14:54:11 +0200

    remotetouch

---
+++ services/remotetouch/index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const path = require('path');
+const router = express.Router();
+
+// Serve static files from client directory
+router.use('/client', express.static(path.join(__dirname, 'client')));
+
+// Route to serve test.html
+router.get('/client/test.html', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client', 'test.html'));
+});
+
+module.exports = {
+  router
+};
